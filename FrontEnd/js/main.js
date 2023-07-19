0a5f4c6a12d6594969168e2ce0cbbae91882c7f4
arthu <arthurfpeixoto@gmail.com>
2023-07-19 16:04:48 -0300

    feature: teste de posição de cursor

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,1 +1,21 @@
 const editor = document.querySelector("#editor")
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var editorDiv = document.getElementById("editor");
+
+    editorDiv.addEventListener("input", function() {
+        var cursorPosition = getCursorPosition();
+        console.log("Posição do cursor: " + cursorPosition);
+    });
+
+    function getCursorPosition() {
+        var selection = window.getSelection();
+        var range = selection.getRangeAt(0);
+        var preSelectionRange = range.cloneRange();
+        preSelectionRange.selectNodeContents(editorDiv);
+        preSelectionRange.setEnd(range.startContainer, range.startOffset);
+        var cursorPosition = preSelectionRange.toString().length;
+        return cursorPosition;
+    }
+});
