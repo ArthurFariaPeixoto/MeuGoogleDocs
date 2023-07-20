e2a50a9b1e3cd84196a918de84eb7b0d2f28e588
arthu <arthurfpeixoto@gmail.com>
2023-07-19 21:08:43 -0300

    feature: armazenamento do texto no localStorage

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,18 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var editorDiv = document.getElementById("editor");
 
-    editorDiv.addEventListener("input", function() {
-        var cursorPosition = getCursorPosition();
-        console.log("Posição do cursor: " + cursorPosition);
+    //Função para pegar o "index" da posição do cursor no texto
+    document.addEventListener("DOMContentLoaded", function() {
+        var editorDiv = document.getElementById("editor");
+
+        editorDiv.addEventListener("input", function() {
+            var cursorPosition = getCursorPosition();
+            console.log("Posição do cursor: " + cursorPosition);
+        });
+
+        function getCursorPosition() {
+            var selection = window.getSelection();
+            var range = selection.getRangeAt(0);
+            var preSelectionRange = range.cloneRange();
+            preSelectionRange.selectNodeContents(editorDiv);
+            preSelectionRange.setEnd(range.startContainer, range.startOffset);
+            var cursorPosition = preSelectionRange.toString().length;
+            return cursorPosition;
+        }
     });
 
-    function getCursorPosition() {
-        var selection = window.getSelection();
-        var range = selection.getRangeAt(0);
-        var preSelectionRange = range.cloneRange();
-        preSelectionRange.selectNodeContents(editorDiv);
-        preSelectionRange.setEnd(range.startContainer, range.startOffset);
-        var cursorPosition = preSelectionRange.toString().length;
-        return cursorPosition;
-    }
-});
+
+    // Carregar conteúdo do editor caso haja algo salvo no armazenamento
+    window.onload = function() {
+        const savedContent = localStorage.getItem('editorContent');
+        if (savedContent) {
+            document.getElementById('editor').innerHTML = savedContent;
+        }
+    };
+
+    // Salva conteúdo do editor no armazenamento local sempre que houver uma alteração
+    document.getElementById('editor').addEventListener('input', function() {
+        const content = this.innerHTML;
+        localStorage.setItem('editorContent', content);
+    });
+
+
+    // O QUE PRECISARIA SER FEITO NESSE CASO SERIA O BACKEND ARMAZENAR A VERSÃO ATUALIZADA DO TEXTO NO LOCALSTORAGE E O FRONTEND MANDAR A VERSÃO ATUAL PARA O BACKEND FAZER UM MERGE
